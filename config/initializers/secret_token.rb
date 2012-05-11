b5e20b53beebf069c87f016398087f85bb0bc71b
Carl Youngblood <carl@youngbloods.org>
2012-05-12 00:26:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equtils::Application.config.secret_token = 'bafa2ca63df6022fc4c0d0d08e7e610db200cf78e8c98224d3ec35920e3fa31f616b3487a65e95999ec5a457dc230fca5dd269c8c4b9d37b704ec15c7b5490af'
